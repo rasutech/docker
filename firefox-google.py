d4fdd4438fa451bacc4c33eee6a4de51a8a5e4fd
rasutech <ping.rln@gmail.com>
2015-10-21 08:28:16 +0530

    Fixes to Python Script

--- firefox-google.py
+++ firefox-google.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -18,12 +19,13 @@ print 'Loaded google.com, going to search Docker in 10 minutes'
 #elems = browser.find_elements_by_tag_name('input');
 elem = browser.find_element_by_id('lst-ib');
 
-elem.send_keys("Docker in 10 minutes");
+elem.send_keys("Docker");
 elem.submit();
 
 if EC.title_contains('Docker in 10 minutes') :
         print 'Successful validation of Google Search using Selenium in Firefox'
 
+time.sleep(5) # Wait for 5 Seconds and then Quit Browser
 browser.quit()
 
 
